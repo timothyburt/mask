9d297ebd473c2d186d05dfe2fd9bc5f96b979987
Chavan Sandhya <gujarati.sandhya@gmail.com>
2022-01-16 23:34:46 -0500

    "Sandhya modified these files on Dt 1/16/2022"

--- calculate.js
+++ calculate.js
@@ -1,6 +1,6 @@
 function Calculate(){
     var NoOfMasks=document.getElementsByName("selectbtn").value;
-    var cost=NoOfMasks*20;
+    var cost = NoOfMasks*20;
     document.getElementsByName("txtbtn").value=cost;
      
 }
